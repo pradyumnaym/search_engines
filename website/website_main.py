1d3cf6d14d0610a0b85d6b3cf92084b00f5cb17c
leviN1998 <levin.kolmar@googlemail.com>
2024-06-30 15:32:50 +0200

    added results page to website. REsults also contain pages now.

--- website/website_main.py
+++ website/website_main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+# import interface to use search engine backend
+from .. import interface
 
 # Run with flask --app website_main run  (--debug)
 app = Flask(__name__)
@@ -10,4 +12,21 @@ def hello_world():
 @app.route("/search", methods=['POST'])
 def search():
     query = request.form.get('query')
-    return f"<p>The query was: {query} </p>"
+    return redirect(url_for('results', query=query))
+
+@app.route("/results")
+def results():
+    query = request.args.get('query')
+    page = int(request.args.get('page', 1))
+    per_page = 10
+    total_results = 100
+
+    results = interface.get_websites(query, count=total_results)
+    
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_results = results[start:end]
+    
+    total_pages = total_results // per_page
+    
+    return render_template('results.html', query=query, results=paginated_results, page=page, total_pages=total_pages)

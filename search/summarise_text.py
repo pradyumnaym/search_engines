b82c76db988f5307af32934d5564205fb9d96cd6
Pradyumna YM <pradyu99914@gmail.com>
2024-07-15 18:01:34 +0200

    summarize text contents based on query.

---
+++ search/summarise_text.py
@@ -0,0 +1,58 @@
+import spacy
+
+try:
+    nlp = spacy.load('en_core_web_lg')
+except OSError:
+    print('Downloading language model for the spaCy POS tagger\n'
+        "(don't worry, this will only happen once)")
+    from spacy.cli import download
+    download('en_core_web_lg')
+    nlp = spacy.load('en_core_web_lg')
+
+def get_relevant_sentences(text, keywords):
+    """
+    Returns top 5 sentences from the text that are most relevant to the keywords.
+
+    Arguments
+    ---------
+    text: str, the text to summarize
+    keywords: str, the keywords to extract from the text
+
+    Returns
+    -------
+    list of str, the top 5 most relevant sentences from the text
+
+    """
+
+    # Keywords for extraction
+    keywords = keywords.split()
+
+    # Process text with spaCy
+    doc = nlp(text)
+
+    # Convert keywords to spaCy tokens
+    keyword_tokens = [nlp(keyword) for keyword in keywords]
+
+    # Function to calculate similarity score for a sentence
+    def sentence_similarity(sentence, keywords):
+        return max(sentence.similarity(keyword) for keyword in keywords)
+
+    sentences = list(doc.sents)
+    sentences.sort(key=lambda sent: sentence_similarity(sent, keyword_tokens), reverse=True)
+
+    # filter out low similarity sentences
+    sentences = [sent for sent in sentences if sentence_similarity(sent, keyword_tokens) > 0.6]
+    sentences = [str(sent) for sent in sentences]
+    
+    return sentences[:5]
+
+if __name__ == "__main__":
+    import random, pickle
+
+    with open("../data/values_sample.pkl", "rb") as f:
+        values = pickle.load(f)
+
+    text = random.choice(values)
+    keywords = "research development"
+
+    print(get_relevant_sentences(text, keywords))

f238208ea89ecef32981ddc5665ba47ed6fd89b1
Julian John <theultimatejj@proton.me>
2024-07-16 22:36:53 +0200

    adding performance testing

--- engine_kernel/combined_results.py
+++ engine_kernel/combined_results.py
@@ -2,7 +2,7 @@ from naive_retriever import get_result
 from rocksdict import Rdict
 from query_postprocessing.related_searches import get_related_searches
 from query_postprocessing.summarise_text import get_relevant_sentences
-
+from datetime import datetime
 
 db = Rdict("../data/crawl_data")
 
@@ -16,19 +16,33 @@ def n_combined_urls(query: str, n: int, search_factor=5) -> list[str]:
 
 
 def n_search_results(query: str, n: int) -> list[tuple[str, str, str]]:
+    retriever_start = datetime.now()
     urls = n_combined_urls(query, n)
+    retriever_end = datetime.now()
 
     results = []
-
-    for url in urls:
+    postprocessing_start = datetime.now()
+    for url in urls[:10]:
         doc = db.get(url)
         related_searches = get_related_searches([doc])
         important_sentences = get_relevant_sentences(doc, query)
         results.append((url, important_sentences, related_searches))
 
+    for url in urls[10:]:
+        results.append((url, "", ""))
+    postprocessing_end = datetime.now()
+
+    print("-------------")
+    print(f"retrieving time: {retriever_end - retriever_start}")
+    print(f"postprocessing time: {postprocessing_end - postprocessing_start}")
+    print(f"total time: {postprocessing_end - retriever_start}")
+    print("-------------")
+
     return results
 
 
 if __name__ == '__main__':
-    results = n_search_results("University Tübingen", 10)
-    print(results)
+    _ = n_search_results("University Tübingen", 10)
+
+    results = n_search_results("Falafel", 100)
+    # print(results)
